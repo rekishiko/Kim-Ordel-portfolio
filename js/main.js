b3ccf081a93a9444eadcbedb9ef91d9c1b3e9da6
Kim Ordel <kim.ordel@gmail.com>
2025-08-31 22:31:57 -0700

    started from scratch. set up colors, created nav bar, created decoration bar, started butoons keep working on nav bar

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// NavBar Link Active
-let links = document.querySelectorAll('.nav-link');
-        for(let i=0; i<links.length; i++){
-        links[i].addEventListener('click', function() {
-            for(let j=0; j<links.length; j++)
-            links[j].classList.remove('active');
-            this.classList.add('active');
-        });
-        }
