b127f450689e0638a81600803172843a0fb39a45
Kim Ordel <kim.ordel@gmail.com>
2025-09-08 09:34:01 -0700

    created html for under construction card. started javascript for inserting card

---
+++ js/underConstructionPopUp.js
@@ -0,0 +1,13 @@
+<script> 
+underConst.addEventListener (click, openPopup);
+  function openPopup(){
+    fetch ("ComingSoonCard.html")
+    .then if(!response.ok) {
+        throw new Error("Problem opening under construction pop up");
+    }
+    .then (openPopup){
+    let const popUp="ComingSoonCard.html"
+    }
+    
+
+</script>
