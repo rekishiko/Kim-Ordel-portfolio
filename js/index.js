35a1da60763bbe14752993e58563d64fb72a845f
Kim Ordel <kim.ordel@gmail.com>
2025-09-12 22:10:41 -0700

    update to all pages. formatting close to finalized for all main page, about, and projects. Carousels added and working to projects page. under construction images added as place holder

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const myCarouselElement = document.querySelectorAll('.carousel');
+
+const carousel = new bootstrap.Carousel(carousel, {
+  interval: false,
+  touch: true,
+  keyboard: true
+})
--- js/underConstructionPopUp.js
+++ js/underConstructionPopUp.js
@@ -1,13 +1,24 @@
-<script> 
-underConst.addEventListener (click, openPopup);
-  function openPopup(){
-    fetch ("ComingSoonCard.html")
-    .then if(!response.ok) {
-        throw new Error("Problem opening under construction pop up");
-    }
-    .then (openPopup){
-    let const popUp="ComingSoonCard.html"
-    }
-    
 
-</script>
+
+  underConst.addEventListener("click", openPopUp);
+  
+  returnbtn.addEventListener("click", closePopup);
+
+  const underConst= document.querySelector(".underConst");
+
+  function openPopUp(){
+    fetch('somePage.html')
+  .then(response => {
+    // When the page is loaded convert it to text
+    return response.text()
+  })
+  .then(html => {
+    // Initialize the DOM parser
+    const popUpHtml = html
+
+  })
+  .catch(error => {
+     console.error('Failed to fetch page: ', error)
+  })
+  }
+
